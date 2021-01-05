4f6efb1da426297f2459fc6fa5f2ea36903c5b8e
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-05 16:27:19 -0300

    chore: add webpack

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts, tsx)$': 'ts-jest'
+    '.+\\.(ts,tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
