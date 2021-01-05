6af8750096c9227c7243a73fc53709c3a1a59d3c
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-05 09:53:22 -0300

    refactor: change imports to use module

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/no-extraneous-dependencies */
 import faker from 'faker';
-import { AuthenticationParams } from '../usecases/authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
