acedc386a2ca66bceddc91bb74852035377328b9
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-05 13:25:52 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+import { HttpPostParams } from '@/data/protocols/http';
+import axios from 'axios';
+
+export default class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url);
+  }
+}
