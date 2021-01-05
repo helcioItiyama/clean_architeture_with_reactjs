6ec4fd8fbb63cb31e38a6c8ae17bcef7ba3a6139
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-05 14:40:20 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,13 @@
 /* eslint-disable class-methods-use-this */
-import { HttpPostParams } from '@/data/protocols/http';
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http';
 import axios from 'axios';
 
-export default class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+export default class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body);
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    };
   }
 }
