511e466e6d3ee00ae4a5fde9a938a6168c1bcab1
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-06 11:19:36 -0300

    test: ensure login starts with submit button disabled

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from './login';
+
+describe('Login Component', () => {
+  it('should start with initial state', () => {
+    const { getByTestId } = render(<Login />);
+
+    const errorWrap = getByTestId('error-wrap');
+    expect(errorWrap.childElementCount).toBe(0);
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
+  });
+});
