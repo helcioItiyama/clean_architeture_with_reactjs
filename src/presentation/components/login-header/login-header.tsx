65f2fb02d0ee30e89146efc0fd0896818bd148e6
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-06 08:15:34 -0300

    refactor: move logo and header to component files

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import Logo from '@/presentation/components/logo/logo';
+import Styles from './login-header-styles.scss';
+
+const LoginHeader: React.FC = () => (
+  <header className={Styles.header}>
+    <Logo />
+    <h1>4Dev - Enquetes para Programadores</h1>
+  </header>
+);
+
+export default memo(LoginHeader);
