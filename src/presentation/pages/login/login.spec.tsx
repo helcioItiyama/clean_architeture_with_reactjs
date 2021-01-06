6f64752e4fc7dad75aba189b9a088da69d122ea3
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-06 15:15:01 -0300

    test: ensure Login calls validation with correct values

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import {
+  render, RenderResult, cleanup, fireEvent,
+} from '@testing-library/react';
+import { Validation } from '@/presentation/protocols/validation';
 import Login from './login';
 
 type SutTypes = {
   sut: RenderResult;
+  validationSpy: ValidationSpy;
 };
 
+class ValidationSpy implements Validation {
+  errorMessage: string;
+
+  input: object;
+
+  validate(input: object):string {
+    this.input = input;
+    return this.errorMessage;
+  }
+}
+
 const makeSut = ():SutTypes => {
-  const sut = render(<Login />);
-  return { sut };
+  const validationSpy = new ValidationSpy();
+  const sut = render(<Login validation={validationSpy} />);
+  return { sut, validationSpy };
 };
 
 describe('Login Component', () => {
+  afterEach(cleanup);
+
   it('should start with initial state', () => {
     const { sut } = makeSut();
 
@@ -28,5 +46,27 @@ describe('Login Component', () => {
     const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Campo Obrigatório');
     expect(passwordStatus.textContent).toBe('🔴');
+  });
+
+  it('should call Validation with correct email', () => {
+    const { sut, validationSpy } = makeSut();
+
+    const emailInput = sut.getByTestId('email');
+    fireEvent.input(emailInput, { target: { value: 'any_email' } });
+
+    expect(validationSpy.input).toEqual({
+      email: 'any_email',
+    });
+  });
+
+  it('should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut();
+
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } });
+
+    expect(validationSpy.input).toEqual({
+      password: 'any_password',
+    });
   });
 });
