86e368388742f1b1d107f8e6cf14cc58e2da7738
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-07 14:24:43 -0300

    Revert "test: ensure EmailValidation returns falsy if email is valid"
    This reverts commit 945604440a0334881a294cac5ab76484dd152748.

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,17 +1,10 @@
 import { InvalidFieldError } from '@/validation/errors';
-import faker from 'faker';
 import { EmailValidation } from './email-validation';
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.random.word());
-    const error = sut.validate(faker.random.word());
+    const sut = new EmailValidation('email');
+    const error = sut.validate('');
     expect(error).toEqual(new InvalidFieldError());
-  });
-
-  it('should return falsy if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word());
-    const error = sut.validate(faker.internet.email());
-    expect(error).toBeFalsy();
   });
 });
