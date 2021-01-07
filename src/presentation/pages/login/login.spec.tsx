aef3834d077769ddc721e317029a551a84df7fc0
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-07 08:56:34 -0300

    test: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -20,7 +20,7 @@ type SutParams = {
   validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({ initialEntries: ['/login'] });
 
 const makeSut = (params?: SutParams):SutTypes => {
   const authenticationSpy = new AuthenticationSpy();
@@ -197,6 +197,9 @@ describe('Login Component', () => {
       'accessToken',
       authenticationSpy.account.accessToken,
     );
+
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe('/');
   });
 
   it('should go to signup page', async () => {
