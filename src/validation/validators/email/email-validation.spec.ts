10eb904c5cd55120bac99301d9a1cf8fedc9c972
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-07 14:49:09 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from '@/validation/errors';
+import faker from 'faker';
 import { EmailValidation } from './email-validation';
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email');
+    const sut = new EmailValidation(faker.random.word());
     const error = sut.validate('');
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  it('should return error if email is invalid', () => {
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
