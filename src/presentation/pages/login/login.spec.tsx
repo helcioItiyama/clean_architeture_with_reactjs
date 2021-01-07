7a3f2bf562c705eebeeeb9842c3f567da8493b03
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-07 08:49:37 -0300

    test: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import faker from 'faker';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import 'jest-localstorage-mock';
 import {
   render, RenderResult, cleanup, fireEvent, waitFor,
@@ -18,13 +20,19 @@ type SutParams = {
   validationError: string;
 };
 
+const history = createMemoryHistory();
+
 const makeSut = (params?: SutParams):SutTypes => {
   const authenticationSpy = new AuthenticationSpy();
 
   const validationStub = new ValidationStub();
   validationStub.errorMessage = params?.validationError;
 
-  const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />);
+  const sut = render(
+    <Router history={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>,
+  );
   return { sut, authenticationSpy };
 };
 
@@ -189,5 +197,14 @@ describe('Login Component', () => {
       'accessToken',
       authenticationSpy.account.accessToken,
     );
+  });
+
+  it('should go to signup page', async () => {
+    const { sut } = makeSut();
+    const register = sut.getByTestId('signup');
+    fireEvent.click(register);
+
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/signup');
   });
 });
