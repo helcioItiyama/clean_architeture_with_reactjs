1d5b626b6b32e3e647866ae48e812d5fe460d6e1
Helcio Itiyama <helcio.itiyama@gmail.com>
2021-01-08 13:02:15 -0300

    refactor: move files to their own files

---
+++ src/main/factories/usecases/remote-authentication-factory.ts
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+import RemoteAuthentication from '@/data/usecases/authentication/remote-authentication';
+import { Authentication } from '@/domain/usecases';
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
+import { makeApiUrl } from '@/main/factories/http/api-url-factory';
+
+export const makeRemoteAuthentication = ():Authentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient());
+};
